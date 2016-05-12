148c53899427ff469feef9bdfaf6477a85e5a95d
Johnwook Choi <johnwook.choi@gmail.com>
2016-05-12 14:16:11 +0900

    Apply style guide to Comment component
    - see https://github.com/airbnb/javascript/tree/master/react for details

---
+++ public/scripts/Comment.jsx
@@ -0,0 +1,38 @@
+import React, {
+  Component,
+  PropTypes,
+} from 'react';
+import marked from 'marked';
+
+const propTypes = {
+  author: PropTypes.string,
+  children: PropTypes.object,
+};
+
+class Comment extends Component {
+  constructor(props) {
+    super(props);
+
+    this.rawMarkup = this.rawMarkup.bind(this);
+  }
+
+  rawMarkup() {
+    const rawMarkup = marked(this.props.children.toString(), { sanitize: true });
+    return { __html: rawMarkup };
+  }
+
+  render() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+}
+
+Comment.propTypes = propTypes;
+
+export default Comment;
